b19d1ee5b3078a01c44812d4ee6af1ef51658d2e
Riquelme Lopes <riquelmeelias0604@gmail.com>
2022-08-07 16:54:52 -0400

    Iniciando a criação da logica de criptografia

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function criptografar() {
   var texto = document.getElementById("texto_digitado").value;
   console.log(texto);
+  console.log(texto.charAt(0));
+  var cript = [];
+    for(var i = 0; i < texto.length; i++){
+      cript.push(texto.charAt(i));
+    }
+    for (var i = 0; i < cript.length; i++) {
+      if(cript[i] == "e"){
+        cript[i] = "enter";
+      }
+    }
+    console.log(cript);
 }
