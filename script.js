9a56ba11bfd94f39a8ae92a283879b00f7307dba
Riquelme Lopes <riquelmeelias0604@gmail.com>
2022-08-09 10:43:37 -0400

    Logica de criptografia básica completa

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 function criptografar() {
   var texto = document.getElementById("texto_digitado").value;
-  console.log(texto);
-  console.log(texto.charAt(0));
+  texto = texto.toLowerCase();
   var cript = [];
     for(var i = 0; i < texto.length; i++){
       cript.push(texto.charAt(i));
     }
-    for (var i = 0; i < cript.length; i++) {
+    for (var i = 0; i < cript.length; i++){
       if(cript[i] == "e"){
         cript[i] = "enter";
       }
+      if(cript[i] == "i"){
+        cript[i] = "imes";
+      }
+      if(cript[i] == "a"){
+        cript[i] = "ai";
+      }
+      if(cript[i] == "o"){
+        cript[i] = "ober";
+      }
+      if(cript[i] == "u"){
+        cript[i] = "ufate";
+      }
     }
     console.log(cript);
+    //var criptString = cript.toString();
+    var criptString = "";
+    for(var m in cript){
+      criptString = criptString.concat(cript[m]);
+    }
+    var escreve = document.getElementById("escreve");
+    escreve.innerHTML = criptString;
+    console.log(criptString);
 }
