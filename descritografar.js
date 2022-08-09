9b2f1ff7822969891f2d5f82f154dd0a5ad19318
Riquelme Lopes <riquelmeelias0604@gmail.com>
2022-08-09 19:22:33 -0400

    Finalizando logica de descriptografar

--- descritografar.js
+++ descritografar.js
@@ -1,9 +1,17 @@
-function descritografar(){
+function descripitografar(){
   var textoDigitado = document.getElementById("texto_digitado").value;
   textoDigitado = textoDigitado.toLowerCase();
   var textoArray = [];
   textoArray = transformaStringEmArray(textoDigitado);
+  console.log(textoArray);
   textoArray = descriptografaTexto(textoArray);
+
+  console.log(textoArray);
+  textoDigitado = transformaArrayEmString(textoArray);
+
+  var escreveNaTela = document.getElementById("escreve");
+  escreveNaTela.innerHTML = textoDigitado;
+  console.log(textoDigitado);
 }
 
 function transformaStringEmArray(texto){
@@ -16,13 +24,6 @@ function transformaStringEmArray(texto){
 
 function descriptografaTexto(texto){
   for (var i = 0; i < texto.length; i++){
-    textoArray.push(texto.charAt(i));
-  }
-  return textoArray;
-}
-
-function criptografarTexto(texto){
-  for (var i = 0; i < texto.length; i++){
     if(texto[i] == "e"){
       i++;
       if(texto[i] == "n"){
@@ -32,27 +33,95 @@ function criptografarTexto(texto){
           if(texto[i] == "e"){
             i++;
             if(texto[i] == "r"){
-              ////excluir lestras da array e substituir por else {
-              ///pode-se transformar em strins em string
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
               }
             }
           }
+        }else{
+          i--;
         }
       }
-      texto[i] = "enter";
-    }
-    if(texto[i] == "i"){
-      texto[i] = "imes";
-    }
-    if(texto[i] == "a"){
-      texto[i] = "ai";
-    }
-    if(texto[i] == "o"){
-      texto[i] = "ober";
-    }
-    if(texto[i] == "u"){
-      texto[i] = "ufate";
-    }
+      if(texto[i] == "i"){
+        i++;
+        if(texto[i] == "m"){
+          i++;
+          if(texto[i] == "e"){
+            i++;
+            if(texto[i] == "s"){
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+            }
+          }
+        }else{
+          i--;
+        }
+      }
+      if(texto[i] == "a"){
+        i++;
+        if(texto[i] == "i"){
+          texto.splice(i, 1);
+          i--;
+        }else{
+          i--;
+        }
+      }
+      if(texto[i] == "o"){
+        i++;
+        if(texto[i] == "b"){
+          i++;
+          if(texto[i] == "e"){
+            i++;
+            if(texto[i] == "r"){
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+              texto.splice(i, 1);
+              i--;
+            }
+          }
+        }else{
+          i--;
+        }
+      }
+      if(texto[i] == "u"){
+        i++;
+        if(texto[i] == "f"){
+          i++;
+          if(texto[i] == "a"){
+            i++;
+            if(texto[i] == "t"){
+                texto.splice(i, 1);
+                i--;
+                texto.splice(i, 1);
+                i--;
+                texto.splice(i, 1);
+                i--;
+            }
+          }
+        }else{
+          i--;
+        }
+      }
   }
   return texto;
+}
+
+function transformaArrayEmString(texto){
+  var textoCriptografado = "";
+  for(var m in texto){
+    textoCriptografado = textoCriptografado.concat(texto[m]);
+  }
+  return textoCriptografado;
 }
